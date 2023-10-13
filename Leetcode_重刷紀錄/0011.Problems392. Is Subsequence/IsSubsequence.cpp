9e5e932409be73108482b11e93a02bd47bb1a9ae
賴祺清 <m4932981@gmail.com>
2023-10-13 23:58:17 +0800

    392. Is Subsequence Cpp

---
+++ Leetcode_重刷紀錄/0011.Problems392. Is Subsequence/IsSubsequence.cpp
@@ -0,0 +1,17 @@
+//392. Is Subsequence
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0,j = 0;
+        while (i < t.size()) {
+            if (s[j] == t[i]) {
+                j++;
+                if(j >= s.size()){
+                    break;
+                }
+            }
+            ++i;
+        }
+        return j == s.size();
+    }
+};
