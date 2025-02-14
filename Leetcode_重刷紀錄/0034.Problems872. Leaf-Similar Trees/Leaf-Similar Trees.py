e53c0d2d89c3768e95343987ea092daea2e8908d
賴祺清 <m4932981@gmail.com>
2025-02-14 17:00:07 +0800

    872. Leaf-Similar Trees

---
+++ Leetcode_重刷紀錄/0034.Problems872. Leaf-Similar Trees/Leaf-Similar Trees.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def get_leaf_sequence(root):
+            leaf_sequence = []
+
+            def dfs(node):
+                if not node:
+                    return
+
+                if not node.left and not node.right:
+                    leaf_sequence.append(node.val)
+                    return
+
+                dfs(node.left)
+                dfs(node.right)
+            dfs(root)
+            return leaf_sequence
+
+        seq1 = get_leaf_sequence(root1)
+        seq2 = get_leaf_sequence(root2)
+        return seq1 == seq2
