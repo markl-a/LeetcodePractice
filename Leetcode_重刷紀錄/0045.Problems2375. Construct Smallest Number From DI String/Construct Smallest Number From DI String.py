daf16ff8add5dfca6819850cdfc70c39794096ec
賴祺清 <m4932981@gmail.com>
2025-02-18 19:54:41 +0800

    2375. Construct Smallest Number From DI String

---
+++ Leetcode_重刷紀錄/0045.Problems2375. Construct Smallest Number From DI String/Construct Smallest Number From DI String.py
@@ -0,0 +1,48 @@
+class Solution:
+    def smallestNumber(self, pattern: str) -> str:
+        n = len(pattern)
+        res = []
+        used = [False] * 10  
+
+        def backtrack(index: int, current_num: int):
+
+
+            if index == n + 1:  
+                return True
+
+            if index == 0: 
+                for i in range(1, 10):
+                    if not used[i]:
+                        used[i] = True
+                        res.append(str(i))
+                        if backtrack(index + 1, i):
+                            return True
+                        used[i] = False  
+                        res.pop()
+                return False
+
+
+            if pattern[index - 1] == 'I':
+                for next_num in range(current_num + 1, 10):  
+                    if not used[next_num]:
+                        used[next_num] = True
+                        res.append(str(next_num))
+                        if backtrack(index + 1, next_num):
+                            return True
+                        used[next_num] = False  
+                        res.pop()
+
+            else:  # pattern[index - 1] == 'D'
+                for next_num in range(1, current_num):  
+                    if not used[next_num]:
+                        used[next_num] = True
+                        res.append(str(next_num))
+                        if backtrack(index + 1, next_num):
+                            return True
+                        used[next_num] = False  
+                        res.pop()
+
+            return False  
+
+        backtrack(0, 0)  
+        return "".join(res)
